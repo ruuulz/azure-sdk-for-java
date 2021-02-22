8f5e94985398a6b38a48855c47595d980c2d2f96
Chidozie Ononiwu <chononiw@microsoft.com>
2021-02-22 10:33:03 -0800

    Sync eng/common directory with azure-sdk-tools repository for Tools PR 1418

--- eng/common/scripts/Prepare-Release.ps1
+++ eng/common/scripts/Prepare-Release.ps1
@@ -2,60 +2,71 @@
 
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$PackageName,
-    [string]$ServiceDirectory,
-    [string]$ReleaseDate, # Pass Date in the form MM/dd/yyyy"
-    [string]$BuildType # For Java
+  [string]$PackageName,
+  [string]$ArtifactName,
+  [string]$ReleaseDate, # Pass Date in the form MM/dd/yyyy"
+  [string]$BuildType # For Java
 )
+
+if (!$PackageName -and !$ArtifactName)
+{
+  LogError "You must specify either PackageName or ArtifactName Argument"
+  return $null
+}
 
 . ${PSScriptRoot}\common.ps1
 
 function Get-ReleaseDay($baseDate)
 {
-    # Find first friday
-    while ($baseDate.DayOfWeek -ne 5)
-    {
-        $baseDate = $baseDate.AddDays(1)
-    }
+  # Find first friday
+  while ($baseDate.DayOfWeek -ne 5)
+  {
+    $baseDate = $baseDate.AddDays(1)
+  }
     
-    # Go to Tuesday
-    $baseDate = $baseDate.AddDays(4)
+  # Go to Tuesday
+  $baseDate = $baseDate.AddDays(4)
 
-    return $baseDate;
+  return $baseDate;
 }
 
 $ErrorPreference = 'Stop'
 
-$packageProperties = Get-PkgProperties -PackageName $PackageName -ServiceDirectory $serviceDirectory
-
-Write-Host "Source directory [ $serviceDirectory ]"
-
-if (!$ReleaseDate)
+if ($PackageName)
 {
-    $currentDate = Get-Date
-    $thisMonthReleaseDate = Get-ReleaseDay((Get-Date -Day 1));
-    $nextMonthReleaseDate = Get-ReleaseDay((Get-Date -Day 1).AddMonths(1));
-
-    if ($thisMonthReleaseDate -ge $currentDate)
-    {
-        # On track for this month release
-        $ParsedReleaseDate = $thisMonthReleaseDate
-    }
-    elseif ($currentDate.Day -lt 15)
-    {
-        # Catching up to this month release
-        $ParsedReleaseDate = $currentDate
-    }
-    else 
-    {
-        # Next month release
-        $ParsedReleaseDate = $nextMonthReleaseDate
-    }
+  $packageProperties = Get-PkgProperties -PackageName $PackageName
 }
 else
 {
-    $ParsedReleaseDate = [datetime]$ReleaseDate
+  $packageProperties = Get-PkgProperties -PackageName $ArtifactName
+}
+
+
+if (!$ReleaseDate)
+{
+  $currentDate = Get-Date
+  $thisMonthReleaseDate = Get-ReleaseDay((Get-Date -Day 1));
+  $nextMonthReleaseDate = Get-ReleaseDay((Get-Date -Day 1).AddMonths(1));
+
+  if ($thisMonthReleaseDate -ge $currentDate)
+  {
+    # On track for this month release
+    $ParsedReleaseDate = $thisMonthReleaseDate
+  }
+  elseif ($currentDate.Day -lt 15)
+  {
+    # Catching up to this month release
+    $ParsedReleaseDate = $currentDate
+  }
+  else 
+  {
+    # Next month release
+    $ParsedReleaseDate = $nextMonthReleaseDate
+  }
+}
+else
+{
+  $ParsedReleaseDate = [datetime]$ReleaseDate
 }
 
 $releaseDateString = $ParsedReleaseDate.ToString("MM/dd/yyyy")
@@ -70,40 +81,40 @@ $newVersion = Read-Host -Prompt "Input the new version, or press Enter to use us
 
 if (!$newVersion)
 {
-    $newVersion = $currentProjectVersion;
+  $newVersion = $currentProjectVersion;
 }
 
 $newVersionParsed = [AzureEngSemanticVersion]::ParseVersionString($newVersion)
 if ($null -eq $newVersionParsed)
 {
-    Write-Error "Invalid version $newVersion. Version must follow standard SemVer rules, see https://aka.ms/azsdk/engsys/packageversioning"
-    exit 1
+  Write-Error "Invalid version $newVersion. Version must follow standard SemVer rules, see https://aka.ms/azsdk/engsys/packageversioning"
+  exit 1
 }
 
 if (Test-Path "Function:SetPackageVersion")
 {
-    SetPackageVersion -PackageName $packageProperties.Name -Version $newVersion -ServiceDirectory $serviceDirectory -ReleaseDate $releaseDateString `
-    -BuildType $BuildType -GroupId $packageProperties.Group
+  SetPackageVersion -ArtifactName $packageProperties.ArtifactName -Version $newVersion -ServiceDirectory $packageProperties.ServiceDirectory `
+    -ReleaseDate $releaseDateString -BuildType $BuildType -GroupId $packageProperties.Group
 }
 else
 {
-    LogError "The function 'SetPackageVersion' was not found.`
+  LogError "The function 'SetPackageVersion' was not found.`
     Make sure it is present in eng/scripts/Language-Settings.ps1.`
     See https://github.com/Azure/azure-sdk-tools/blob/master/doc/common/common_engsys.md#code-structure"
-    exit 1
+  exit 1
 }
 
 &$EngCommonScriptsDir/Update-DevOps-Release-WorkItem.ps1 `
--language $LanguageDisplayName `
--packageName $packageProperties.Name `
--version $newVersion `
--plannedDate $releaseDateString `
--packageRepoPath $packageProperties.serviceDirectory
+  -language $LanguageDisplayName `
+  -packageName $packageProperties.Name `
+  -version $newVersion `
+  -plannedDate $releaseDateString `
+  -packageRepoPath $packageProperties.erviceDirectory
 
 git diff -s --exit-code $packageProperties.DirectoryPath
 if ($LASTEXITCODE -ne 0)
 {
-    git status
-    Write-Host "Some changes were made to the repo source" -ForegroundColor Green
-    Write-Host "Submit a pull request with the necessary changes to the repo" -ForegroundColor Green
+  git status
+  Write-Host "Some changes were made to the repo source" -ForegroundColor Green
+  Write-Host "Submit a pull request with the necessary changes to the repo" -ForegroundColor Green
 }
